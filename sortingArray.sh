31db8cd272f08d48e292f5541e03ae3d2df8c67f
darpan2212 <darpan.madlani@gmail.com>
2021-09-18 19:39:36 +0530

    stored values in dictionary

--- sortingArray.sh
+++ sortingArray.sh
@@ -1,11 +1,12 @@
+declare -A math;
 read -p "Enter value a: " a;
 read -p "Enter value b: " b
 read -p "Enter value c: " c
 
-computation1=$(( a+b*c ));
-computation2=$(( a*b+c ));
-computation3=$(( c+a/b ));
-computation4=$(( a%b+c ));
+math[calc1]=$(( a+b*c ));
+math[calc2]=$(( a*b+c ));
+math[calc3]=$(( c+a/b ));
+math[calc4]=$(( a%b+c ));
 
 echo $a $b $c;
-echo $computation1 $computation2 $computation3;
+echo ${math[@]};
