ea6cef6079b07e788ff087ef1aa62df8749758e5
darpan2212 <darpan.madlani@gmail.com>
2021-09-18 19:51:45 +0530

    dictionary to be store in array

--- sortingArray.sh
+++ sortingArray.sh
@@ -8,5 +8,11 @@ math[calc2]=$(( a*b+c ));
 math[calc3]=$(( c+a/b ));
 math[calc4]=$(( a%b+c ));
 
+for ((cnt=0;cnt<${#math[@]};cnt++))
+do
+	varArray[cnt]=${math[calc"$(( cnt+1 ))"]};
+done
+
 echo $a $b $c;
 echo ${math[@]};
+echo ${varArray[@]};
